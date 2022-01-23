aa1cc6dfb7bb2c303c39d262d3079adcf8b1dda3
Yoshi <h.yoshikazu0110@gmail.com>
2022-01-24 00:43:33 +0900

    環境構築　react から rails apiを呼び出す準備 axios http通信を行うライブラリのインストール

--- front/react-sample/src/index.js
+++ front/react-sample/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Sample from './components/Sample';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Sample />
   </React.StrictMode>,
   document.getElementById('root')
 );
