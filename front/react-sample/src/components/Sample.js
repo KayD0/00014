4656a25591431e63d360f8f516ecc1076cea1124
Yoshi <h.yoshikazu0110@gmail.com>
2022-01-29 01:13:18 +0900

    環境構築　react から rails apiを呼び出す準備 「CORS（クロス・オリジン・リソース・シェアリング）」の設定、の続き ReactからRailsのapiを呼び出し、JsonデータをViewに表示させるまでの処理は作成 https://awesomecatsis.com/useeffect/

--- front/react-sample/src/components/Sample.js
+++ front/react-sample/src/components/Sample.js
@@ -3,21 +3,49 @@ import axios from 'axios';
 
 function Sample() {
   
-  const [ret, setRet] = useState([]);
+  const [sample, setSamples] = useState([]);
+  const [ loading, setLoading] = useState(false)
+  const [ error, setError] = useState(false)
 
   useEffect(() => {
-    async function fetchData() {
-    const response = await axios.get('http://localhost:3001/api/v1/sample');
-    setRet(response.data.results);
-    return response;
-  }
-  fetchData();
-}, []);
-  
+    setLoading(true)
+    setError(false)
+    axios.get(`http://localhost:3001/api/v1/sample`)
+    .then((res) => {
+      console.log(res)
+        const sampleData = res.data
+        setSamples(sampleData)
+        setLoading(false)
+    })
+    .catch(() => {
+      setError(true)
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  }, []);
+
   return (
     <div className="Sample">
         <p>Hello World!</p>
-        {ret}
+        {
+          error ? (
+            <p style={{color: "red"}} >
+              データを取得できませんでした。
+            </p>
+          )
+          :loading ? (
+            <p style={{color: "green"}}>
+              loading...
+            </p>
+          )
+          :(
+            <div>
+              <p>{sample.status}</p>
+              <p>{sample.message}</p>
+            </div>
+          )
+        }
     </div>
   );
 }
